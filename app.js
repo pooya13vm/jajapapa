bf50a4d54f01cbdaedb9949a558cb54555c2e013
pooya <pooya13vm@gmail.com>
2022-01-06 15:48:27 +0300

    access code generated

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ document.addEventListener("click", function (e) {
   }
 });
 
-const accessCode = 123456;
 const form = document.getElementById("form");
 const personalTitle = document.getElementById("personal-title");
 const cards = document.querySelectorAll(".edu-img");
@@ -31,13 +30,14 @@ window.addEventListener("scroll", function () {
     scrollBtn.classList.remove("is-visible");
   }
 });
-console.log(window.scrollY);
+
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   const inputCode = document.getElementById("inputCode").value;
   const inputName = document.getElementById("inputName").value;
   const message = document.querySelector(".message");
-  if (inputCode == accessCode || document.cookie) {
+  const checker = accessCode.find((x) => x.accessCODE == inputCode);
+  if (checker || document.cookie) {
     document.querySelector(".modal-overlay").style.display = "none";
     personalTitle.innerHTML = `Hi ${inputName}`;
     document.cookie = `name=${inputName};Secure`;
@@ -57,3 +57,30 @@ for (let i = 0; i < cards.length; i++) {
 scrollBtn.addEventListener("click", function () {
   window.scroll({ top: 0, left: 0, behavior: "smooth" });
 });
+
+// -----------------access code generator------------
+
+// const jasonMaker = () => {
+//   const myArray = [];
+//   for (let i = 0; i < 500; i++) {
+//     let accessCodeArray = [];
+//     for (let j = 0; j < 6; j++) {
+//       let code = Math.floor(Math.random() * 10);
+//       accessCodeArray.push(code);
+//     }
+//     let generatedItem = accessCodeArray.join("");
+//     if (myArray.indexOf(generatedItem) == -1) {
+//       myArray.push(generatedItem);
+//     } else {
+//       i--;
+//     }
+//   }
+//   const myJson = [];
+//   for (let k = 0; k < 500; k++) {
+//     let costumerObj = { id: k, email: "", accessCODE: myArray[k], used: false };
+//     myJson.push(costumerObj);
+//   }
+//   let jsonFile = JSON.stringify(myJson);
+//   console.log(jsonFile);
+// };
+// jasonMaker();
