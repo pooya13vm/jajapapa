05f33ba8c77406daf14f43df1b88908214fe7ea8
pooya <pooya13vm@gmail.com>
2022-02-14 20:46:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ const scrollBtn = document.getElementById("scroll-top");
 const getNameFromCookie = () => {
   let cookie = document.cookie;
   let indexOfName = cookie.indexOf("name");
+  console.log(indexOfName);
   let indexOfFirstChar = cookie.indexOf(";", indexOfName);
+  console.log(indexOfFirstChar);
   if (indexOfFirstChar == -1) {
     return cookie.slice(5, cookie.length);
   } else {
     let myCookie = cookie.slice(indexOfName, indexOfFirstChar);
+    console.log(myCookie);
     return myCookie.slice(5, myCookie.length);
   }
 };
